423626c45005e935b2d82a52f14f13f8c640cf69
Samyak Jain <samjain0987654@gmail.com>
2026-09-14 11:15:32 +0530

    Implement swap of two integers using arithmetic

---
+++ day4_code7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int first, second;
+
+    scanf("%d %d", &first, &second);
+
+    first = first + second;
+    second = first - second;
+    first = first - second;
+
+    printf("After swap: %d %d\n", first, second);
+
+    return 0;
+}
